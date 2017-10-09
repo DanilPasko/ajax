0da73ebbc7bb2d72f85471544d31ec39ef5a8890
Danil <danilpasko@gmail.com>
2017-10-09 14:48:35 +0300

    weather app

--- proj/main.js
+++ proj/main.js
@@ -1,27 +1,28 @@
 $('button').on('click', function(){
 	var xhr = new XMLHttpRequest();
-
-	xhr.open('GET', 'phones.json', true);
+	var city = $(this).attr("value");
+	var adress = 'http://api.openweathermap.org/data/2.5/weather?q='+ city + '&APPID=ade7221313e248ee596b11990178251d';
+	
+	xhr.open('GET', adress, true);
 
 	xhr.send(); // (1)
 
 	xhr.onreadystatechange = function() { // (3)
 	  	if (xhr.readyState != 4) return;
 
-	  		$('button').innerHTML = 'Готово!';
+	  		// $('button').innerHTML = 'Готово!';
 
 	  	if (xhr.status != 200) {
 		    alert(xhr.status + ': ' + xhr.statusText);
 	  	} else {
 	    	var a = xhr.responseText;
 	    	var b = JSON.parse(a);
-	    	for (var i = 0; i < b.length; i++) {
-	    		console.log(b[i].id + ': ' + b[i].name);
-	    	}
-	  	}
+	    	var resultTemp = Math.round(b.main.temp - 273,15);
+	    	$('.resultTemp').text(resultTemp + 'C');
 
-	}
-
-	$('button').innerHTML = 'Загружаю...'; // (2)
-	$('button').disabled = true;
+	    	var resultWeather = b['weather'][0].main;
+	    	$('.resultWeather').text(resultWeather);
+	  	};
+	};
 });
+
